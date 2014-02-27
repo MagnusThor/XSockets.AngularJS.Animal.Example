26b38d1ba195d15048950b841d58e5c2d959ac44
Magnus Thor <magnus@xsockets.net>
2014-02-27 19:35:01 +0100

    Fixing routes etc

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -1,6 +1,6 @@
 ﻿var animalApp = angular.module('animalApp', ['XSockets']).
   config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
       $routeProvider.
-          when('/animals/', { templateUrl: '/app/partials/animals.html', controller: AnimalsController }).
+          when('/animals/', { templateUrl: 'app/partials/animals.html', controller: AnimalsController }).
           otherwise({ redirectTo: '/animals' });
   }]);
